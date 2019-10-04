a4fb7fefa08af3e971490b08a230f27da808661d
Xiuming Li <xiumingli610@gmail.com>
2019-10-04 11:47:29 -0400

    Xiuming Li changes

--- app/src/main/java/com/example/tiletakeover/Player.java
+++ app/src/main/java/com/example/tiletakeover/Player.java
@@ -2,7 +2,6 @@ package com.example.tiletakeover;
 
 public class Player {
     public static void main(String[]args){
-        System.out.println("hi");
         System.out.println("hello world");
         System.out.println("1");
     }
