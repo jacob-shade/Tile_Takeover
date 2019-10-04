a26b499cde28bee374d495f5a36d70cd0cd9ed54
Rkerk <robertkerkula26@gmail.com>
2019-10-04 11:48:51 -0400

    added Player class

--- app/src/main/java/com/example/tiletakeover/Player.java
+++ app/src/main/java/com/example/tiletakeover/Player.java
@@ -3,6 +3,6 @@ package com.example.tiletakeover;
 public class Player {
     public static void main(String[]args){
         System.out.println("hello world");
-        System.out.println("1");
+        System.out.println("47");
     }
 }
