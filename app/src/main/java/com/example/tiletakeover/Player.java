816c872e5a8a9665513a64e348f1f993572d1f3e
Rkerk <robertkerkula26@gmail.com>
2019-10-30 10:17:24 -0400

    test

--- app/src/main/java/com/example/tiletakeover/Player.java
+++ app/src/main/java/com/example/tiletakeover/Player.java
@@ -1,13 +1,22 @@
 package com.example.tiletakeover;
 
 public class Player {
-    public static void main(String[]args){
-        System.out.println("hello world");
-        System.out.println("49");
-        System.out.println("commit then push");
 
-        System.out.println("this is a test");
-        System.out.println("this is a test1");
-        System.out.println("this is a test li");
+    String playerName;
+    int position;
+    int[]posMoves;
+    boolean winTile;
+    boolean pass;
+    /**
+     * playerName represents the name that will be displayed for each player
+     * position represents the integer position of each player on the board
+     * int[]posMoves represents all of the possible legal moves for the current player
+     * winTile is a flag that will be used to show that the player has landed on the winning tile
+     * pass is a flag that will determine when a player's turn ends
+     */
+
+
+    public Player(){
+
     }
 }
