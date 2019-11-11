6aa419939acc704e4f02e4f3647c988fae472c08
shadejacob <shadejacob@gmail.com>
2019-11-11 09:47:40 -0500

    cleaned up the if statements for displaying tiles, updated pictures for tiles

--- app/src/main/java/com/example/tiletakeover/Board.java
+++ app/src/main/java/com/example/tiletakeover/Board.java
@@ -90,106 +90,34 @@ public class Board {
         Button button;
         for(int i = 0; i < boardTiles.length; i++) {
             button = new Button(context);
-            if(boardTiles[i].equals("0")) {
-                button.setBackgroundResource(R.drawable.player_tile);
-            } else if (boardTiles[i].equals("1")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("2")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("3")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("4")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("5")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("6")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("7")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("8")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("9")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("10")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("11")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("12")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("13")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("14")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("15")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("16")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("17")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("18")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("19")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("20")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("21")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("22")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("23")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("24")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("25")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("26")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("27")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            }else if (boardTiles[i].equals("28")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("29")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("30")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("31")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("32")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("33")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("34")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("35")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("36")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("37")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            }else if (boardTiles[i].equals("38")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("39")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("40")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("41")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("42")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("43")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("44")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("45")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("46")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("47")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            }else if (boardTiles[i].equals("48")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
-            } else if (boardTiles[i].equals("49")) {
-                button.setBackgroundResource(R.drawable.blank_tile);
+            if(boardTiles[i].equals("0") || boardTiles[i].equals("48")) {
+                button.setBackgroundResource(R.drawable.player);
+            } else if (boardTiles[i].equals("1") || boardTiles[i].equals("5") ||
+                    boardTiles[i].equals("6") || boardTiles[i].equals("7") ||
+                    boardTiles[i].equals("13") || boardTiles[i].equals("35") ||
+                    boardTiles[i].equals("41") || boardTiles[i].equals("42") ||
+                    boardTiles[i].equals("43") || boardTiles[i].equals("47")) {
+                button.setBackgroundResource(R.drawable.platform);
+            } else if (boardTiles[i].equals("2") || boardTiles[i].equals("3") ||
+                    boardTiles[i].equals("4") || boardTiles[i].equals("8") ||
+                    boardTiles[i].equals("9") || boardTiles[i].equals("10") ||
+                    boardTiles[i].equals("11") || boardTiles[i].equals("12") ||
+                    boardTiles[i].equals("14") || boardTiles[i].equals("15") ||
+                    boardTiles[i].equals("16") || boardTiles[i].equals("17") ||
+                    boardTiles[i].equals("18") || boardTiles[i].equals("19") ||
+                    boardTiles[i].equals("20") || boardTiles[i].equals("21") ||
+                    boardTiles[i].equals("22") || boardTiles[i].equals("23") ||
+                    boardTiles[i].equals("24") || boardTiles[i].equals("25") ||
+                    boardTiles[i].equals("26") || boardTiles[i].equals("27") ||
+                    boardTiles[i].equals("28") || boardTiles[i].equals("29") ||
+                    boardTiles[i].equals("30") || boardTiles[i].equals("31") ||
+                    boardTiles[i].equals("32") || boardTiles[i].equals("33") ||
+                    boardTiles[i].equals("34") || boardTiles[i].equals("36") ||
+                    boardTiles[i].equals("37") || boardTiles[i].equals("38") ||
+                    boardTiles[i].equals("39") || boardTiles[i].equals("40") ||
+                    boardTiles[i].equals("44") || boardTiles[i].equals("45") ||
+                    boardTiles[i].equals("46")){
+                button.setBackgroundResource(R.drawable.empty);
             }
             buttons.add(button);
         }
